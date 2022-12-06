49ae276a14215cd16d636439aa744b3c3fd9e0e1
Ovuoke Aghwotu <aghwotuovuoke@gmail.com>
2022-12-06 02:17:09 +0100

    feat: setup firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBcOfLB6USpLdwIQAjkUBoSm4ZtZch4TPE',
+    authDomain: 'clips-project-23796.firebaseapp.com',
+    projectId: 'clips-project-23796',
+    storageBucket: 'clips-project-23796.appspot.com',
+    // messagingSenderId: '10971108189',
+    appId: '1:10971108189:web:c00cedb8720a051e864eb4',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBcOfLB6USpLdwIQAjkUBoSm4ZtZch4TPE',
+    authDomain: 'clips-project-23796.firebaseapp.com',
+    projectId: 'clips-project-23796',
+    storageBucket: 'clips-project-23796.appspot.com',
+    // messagingSenderId: '10971108189',
+    appId: '1:10971108189:web:c00cedb8720a051e864eb4',
+  },
 };
 
 /*
