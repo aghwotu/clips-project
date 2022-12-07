4bda3356529a3521d7cde14620fb1682915bf4a9
Ovuoke Aghwotu <aghwotuovuoke@gmail.com>
2022-12-07 21:52:06 +0100

    feat: add type safety when creating a new user

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,20 +1,30 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { AngularFirestore } from '@angular/fire/compat/firestore';
+import {
+  AngularFirestore,
+  AngularFirestoreCollection,
+} from '@angular/fire/compat/firestore';
 import IUser from '../models/user.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor(private _auth: AngularFireAuth, private _db: AngularFirestore) {}
+  private _usersCollection: AngularFirestoreCollection<IUser>;
+
+  constructor(private _auth: AngularFireAuth, private _db: AngularFirestore) {
+    this._usersCollection = this._db.collection('users');
+  }
 
   public async createUser(userData: IUser) {
+    if (!userData.password) {
+      throw new Error('Password not provided.');
+    }
     const userCredentials = await this._auth.createUserWithEmailAndPassword(
       userData.email,
       userData.password
     );
-    await this._db.collection('users').add({
+    await this._usersCollection.add({
       name: userData.name,
       email: userData.email,
       age: userData.age,
