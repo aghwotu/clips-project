106617e134486daad0687667d805aae6415263a6
Ovuoke Aghwotu <aghwotuovuoke@gmail.com>
2022-12-07 23:02:40 +0100

    feat: connect authenticated user to account in database

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -24,11 +24,19 @@ export class AuthService {
       userData.email,
       userData.password
     );
-    await this._usersCollection.add({
+
+    if (!userCredentials.user) {
+      throw new Error("User can't be found.");
+    }
+    await this._usersCollection.doc(userCredentials.user?.uid).set({
       name: userData.name,
       email: userData.email,
       age: userData.age,
       phoneNumber: userData.phoneNumber,
+    });
+
+    await userCredentials.user.updateProfile({
+      displayName: userData.name,
     });
   }
 }
