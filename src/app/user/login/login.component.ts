3760da16f528b314315c3264d1253137aad1f4fe
Ovuoke Aghwotu <aghwotuovuoke@gmail.com>
2022-12-08 12:42:23 +0100

    feat: setup login with email and password

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'app-login',
@@ -10,12 +11,36 @@ export class LoginComponent implements OnInit {
     email: '',
     password: '',
   };
+  showAlert: boolean = false;
+  alertMessage: string = 'Please wait! We are logging you in.';
+  alertColor: string = 'blue';
+  formInSubmission: boolean = false;
 
-  constructor() {}
+  constructor(private _auth: AngularFireAuth) {}
 
   ngOnInit(): void {}
 
-  login() {
-    console.log(this.credentials);
+  async login() {
+    this.showAlert = true;
+    this.alertMessage = 'Please wait! We are logging you in.';
+    this.alertColor = 'blue';
+    this.formInSubmission = true;
+
+    const { email, password } = this.credentials;
+    try {
+      await this._auth.signInWithEmailAndPassword(email, password);
+    } catch (error) {
+      this.formInSubmission = false;
+      this.alertMessage =
+        'An unexpected error occured. Please try again later.';
+      this.alertColor = 'red';
+      console.error(error);
+
+      throw new Error('An error occured');
+      return;
+    }
+
+    this.alertMessage = 'Success! You are now logged in.';
+    this.alertColor = 'green';
   }
 }
