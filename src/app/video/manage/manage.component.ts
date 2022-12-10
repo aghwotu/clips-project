8777200890fece02bbdfdf5fe343e94b62226094
Ovuoke Aghwotu <aghwotuovuoke@gmail.com>
2022-12-10 18:17:44 +0100

    feat: persisting the selected option in dropdown

--- src/app/video/manage/manage.component.ts
+++ src/app/video/manage/manage.component.ts
@@ -7,18 +7,19 @@ import { Router, ActivatedRoute, Params } from '@angular/router';
   styleUrls: ['./manage.component.scss'],
 })
 export class ManageComponent implements OnInit {
-  videoOrder = '1';
+  videoOrder = '1'; //we are storing this as a string because it is the default type from input fields
 
   constructor(private _router: Router, private _route: ActivatedRoute) {}
 
   ngOnInit() {
     this._route.queryParamMap.subscribe((params: Params) => {
-      this.videoOrder = params['sort'] === '2' ? params['sort'] : '1';
+      this.videoOrder =
+        params['params']['sort'] === '2' ? params['params']['sort'] : '1';
     });
   }
 
-  sort($event: Event) {
-    const { value } = $event?.target as HTMLSelectElement;
+  sort(event: Event) {
+    const { value } = event.target as HTMLSelectElement;
     // option 1
     // this._router.navigateByUrl(`/manage?sort=${value}`);
 
