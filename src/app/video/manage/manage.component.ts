077d968a6b2fb0f1ede75ede88e6889bba185de6
Ovuoke Aghwotu <aghwotuovuoke@gmail.com>
2022-12-10 17:16:27 +0100

    feat: updating the query parameters in our url

--- src/app/video/manage/manage.component.ts
+++ src/app/video/manage/manage.component.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-manage',
   templateUrl: './manage.component.html',
   styleUrls: ['./manage.component.scss'],
 })
-export class ManageComponent {}
+export class ManageComponent implements OnInit {
+  videoOrder = '1';
+
+  constructor(private _router: Router, private _route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this._route.queryParamMap.subscribe((params: Params) => {
+      this.videoOrder = params['sort'] === '2' ? params['sort'] : '1';
+    });
+  }
+
+  sort($event: Event) {
+    const { value } = event?.target as HTMLSelectElement;
+    this._router.navigateByUrl(`/manage?sort=${value}`);
+  }
+}
