ef8a0c9fa043b04864ea14e0f3faeb43e55c7854
Ovuoke Aghwotu <aghwotuovuoke@gmail.com>
2022-12-10 20:31:38 +0100

    feat: add drag and drop support to the file upload

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-upload',
   templateUrl: './upload.component.html',
-  styleUrls: ['./upload.component.scss']
+  styleUrls: ['./upload.component.scss'],
 })
 export class UploadComponent {
+  isDragover: boolean = false;
 
+  storeFile(event: Event) {
+    this.isDragover = false;
+  }
 }
