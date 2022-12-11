a5952f4c5a00a0d01bc78e42fe1ab70cebccfc96
Ovuoke Aghwotu <aghwotuovuoke@gmail.com>
2022-12-11 17:25:30 +0100

    feat: querying the firebase database

--- src/app/video/manage/manage.component.ts
+++ src/app/video/manage/manage.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
+import { ClipService } from 'src/app/services';
 
 @Component({
   selector: 'app-manage',
@@ -9,13 +10,19 @@ import { Router, ActivatedRoute, Params } from '@angular/router';
 export class ManageComponent implements OnInit {
   videoOrder = '1'; //we are storing this as a string because it is the default type from input fields
 
-  constructor(private _router: Router, private _route: ActivatedRoute) {}
+  constructor(
+    private _router: Router,
+    private _route: ActivatedRoute,
+    private _clipService: ClipService
+  ) {}
 
   ngOnInit() {
     this._route.queryParamMap.subscribe((params: Params) => {
       this.videoOrder =
         params['params']['sort'] === '2' ? params['params']['sort'] : '1';
     });
+
+    this._clipService.getUserClips().subscribe(console.log);
   }
 
   sort(event: Event) {
