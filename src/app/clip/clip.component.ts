d523fb6f55e848465ea352e7ad2bdbf540b0c443
Ovuoke Aghwotu <aghwotuovuoke@gmail.com>
2022-12-13 21:37:02 +0100

    feat: turnoff ViewEncapsulation so no unique ids are added to elements
    This will allow our video player styles to work

--- src/app/clip/clip.component.ts
+++ src/app/clip/clip.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  ViewEncapsulation,
+} from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import videojs from 'video.js';
 
@@ -6,6 +12,7 @@ import videojs from 'video.js';
   selector: 'app-clip',
   templateUrl: './clip.component.html',
   styleUrls: ['./clip.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class ClipComponent implements OnInit {
   id: string = '';
