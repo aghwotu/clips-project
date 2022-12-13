30d24c2a64ea776fefa672b212d7de4c5559b865
Ovuoke Aghwotu <aghwotuovuoke@gmail.com>
2022-12-13 18:07:17 +0100

    feat: scroll event to detect if we've scrolled to the bottom of the page window

--- src/app/clips-list/clips-list.component.ts
+++ src/app/clips-list/clips-list.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-clips-list',
   templateUrl: './clips-list.component.html',
-  styleUrls: ['./clips-list.component.scss']
+  styleUrls: ['./clips-list.component.scss'],
 })
-export class ClipsListComponent {
+export class ClipsListComponent implements OnInit, OnDestroy {
+  ngOnInit(): void {
+    window.addEventListener('scroll', this.handleScroll);
+  }
 
+  handleScroll = () => {
+    const { scrollTop, offsetHeight } = document.documentElement;
+    const { innerHeight } = window;
+
+    const bottomOfWindow = Math.round(scrollTop) + innerHeight === offsetHeight;
+
+    if (bottomOfWindow) {
+      console.log('we are at the bottom of the window');
+    }
+  };
+
+  ngOnDestroy(): void {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
 }
