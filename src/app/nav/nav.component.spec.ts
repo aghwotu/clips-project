57bc16ade4f62c381ecc31b0b79d82f934aaa19a
Ovuoke Aghwotu <aghwotuovuoke@gmail.com>
2022-12-14 23:50:34 +0100

    test: create mock service in test

--- src/app/nav/nav.component.spec.ts
+++ src/app/nav/nav.component.spec.ts
@@ -1,16 +1,21 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
 
 import { NavComponent } from './nav.component';
 
 describe('NavComponent', () => {
   let component: NavComponent;
   let fixture: ComponentFixture<NavComponent>;
+  const mockedAuthService = jasmine.createSpyObj(
+    'AuthService',
+    ['createUser', 'logout'],
+    { isAuthenticated$: of(true) }
+  );
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ NavComponent ]
-    })
-    .compileComponents();
+      declarations: [NavComponent],
+    }).compileComponents();
   });
 
   beforeEach(() => {
